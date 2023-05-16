78dc0aaafd81e59753f3d87702f240dc81c9e686
Matheus Ricardo <matheu_ric99@hotmail.com>
2023-05-15 21:21:22 -0300

    add search

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,19 @@
 import { Head, LogoDiv, Logo, Search, Input, Button } from "./styles";
 import logo from "../../assets/logo.png";
 
-const Header = () => {
+const Header = ({ filteredProducts, setFilteredProducts }) => {
   return (
     <Head>
       <LogoDiv>
         <Logo src={logo} alt="" />
       </LogoDiv>
       <Search>
-        <Input type="text" placeholder="Digite sua pesquisa" />
+        <Input
+          type="text"
+          placeholder="Digite sua pesquisa"
+          value={filteredProducts}
+          onChange={(event) => setFilteredProducts(event.target.value)}
+        />
         <Button>Pesquisar</Button>
       </Search>
     </Head>
